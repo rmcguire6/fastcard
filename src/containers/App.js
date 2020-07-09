960ad78d013818539b311c666043d06f6668b083
Regina McGuire <rmcguire@antelecom.net>
2020-07-08 18:26:26 -0700

    Add form to add a match

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,42 @@
-import React, {useState, useEffect} from 'react';
-import Match from './Match'
-import VerbsContext from '../context/verbs-context'
-import words from '../fixtures/words'
+import React, {useState} from 'react';
 import '../App.css'
-function App() {
-  const [verbs, setVerbs] = useState([])
-  useEffect(() => {
-    setVerbs(words)
-  }, [])
+const App = () => {
+  const [matches, setMatches] = useState([])
+  const [english, setEnglish] = useState('')
+  const [spanish, setSpanish] = useState('')
   
+  const addMatch = (e) => {
+    e.preventDefault()
+    setMatches([
+      ...matches,
+      {
+        english,
+        spanish
+      }
+    ])
+    setEnglish('')
+    setSpanish('')
+  }
   return (
-    <VerbsContext.Provider value={{verbs}}>
     <div className="App">
       <header className="App-header">
        <h1>Fast Card</h1>
       </header>
       <p>Learn faster with Fast Card</p>
-      < Match />
+      <h2>Match</h2>
+      {matches.map((match) => (
+        <div key={match.spanish}>
+          <h4>{`to ${match.english}`}</h4>
+          <h4>{match.spanish}</h4>
+        </div>
+      ))}
+      <h3>Add a Word Match</h3>
+      <form onSubmit={addMatch}>
+        <input value={english} onChange={(e) => setEnglish(e.target.value)}/>
+        <input value={spanish} onChange={(e) => setSpanish(e.target.value)}/>
+        <button >Add a word pair</button>
+      </form>
     </div>
-    </VerbsContext.Provider>
   );
 }
 
