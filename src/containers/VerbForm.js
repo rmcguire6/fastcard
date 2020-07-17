e3316357ff191f762fd3b7861d02152797532a53
Regina McGuire <rmcguire@antelecom.net>
2020-07-16 21:43:57 -0700

    Refactor Verb Form to add click handlers

--- src/containers/VerbForm.js
+++ src/containers/VerbForm.js
@@ -1,46 +1,67 @@
-import React, {useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import Card from '../components/Card'
-
 import '../App.css'
 
-const VerbForm= () => {
+const VerbForm = () => {
+  const modelMatch = { match_id: uuidv4, spanish: 'vivir', english: 'live' }
   const [spanishList, setSpanishList] = useState([])
   const [englishList, setEnglishList] = useState([])
   const createSpanishTenses = (inf, conj) => {
     let spanishTenses = []
-    const stem = inf.slice(0, inf.length-1)
-    const shortstem = inf.slice(0, inf.length-2)
-    if ( conj !== 'ir') {
-      spanishTenses = [`${shortstem}o`,`${stem}s`,`${stem}`,`${stem}mos`,`${stem}is`,`${stem}n`]
+    const stem = inf.slice(0, inf.length - 1)
+    const shortstem = inf.slice(0, inf.length - 2)
+    if (conj !== 'ir') {
+      spanishTenses = [{ pers: '1s', tense: `yo ${shortstem}o` },
+        { pers: '2s', tense: `tu ${stem}s` }, { pers: '3s', tense: `el ${stem}` },
+        { pers: '1p', tense: `nosotros ${stem}mos` }, { pers: '2p', tense: `vosotros ${stem}is` },
+        { pers: '3p', tense: `ellos ${stem}n` }]
     } else {
-      spanishTenses = [`${shortstem}o`,`${shortstem}es`,`${shortstem}e`,`${stem}mos`,`${shortstem}is`,`${shortstem}en`]
+      spanishTenses = spanishTenses = [{ pers: '1s', tense: `yo ${shortstem}o` },
+        { pers: '2s', tense: `tu ${shortstem}es` }, { pers: '3s', tense: `el ${shortstem}e` },
+        { pers: '1p', tense: `nosotros ${stem}mos` }, { pers: '2p', tense: `vosotros ${stem}s` },
+        { pers: '3p', tense: `ellos ${shortstem}en` }]
     }
     return spanishTenses
   }
   const createEnglishTenses = (inf) => {
-    return [`I ${inf}`, `you ${inf}`, `he or she ${inf}s`, `we ${inf}`, `you(pl) ${inf}`, `they ${inf}`]
+    return [{ pers: '1s', tense: `I ${inf}` },
+      { pers: '2s', tense: `you ${inf}` }, { pers: '3s', tense: `he or she ${inf}s` },
+      { pers: '1p', tense: `we ${inf}` }, { pers: '2p', tense: `you(pl) ${inf}` },
+      { pers: '3p', tense: `they ${inf}` }]
   }
-  useEffect(()=> {
-    setSpanishList(createSpanishTenses('hablar', 'ar'))
-    setEnglishList(createEnglishTenses('speak'))
+  useEffect(() => {
+    setSpanishList(createSpanishTenses(modelMatch.spanish, 'ir'))
+    setEnglishList(createEnglishTenses(modelMatch.english))
   }, [])
-
-return (
-  <div className="container">
-    <h2>Match the Verb Forms</h2>
-    <div className="lists">
-      <div className="list">
-      {spanishList.map((word, index) => (
-        <Card key={index} word={word} />
-      ))}
-      </div>
-      <div className="list">
-      {englishList.map((word, index) => (
-        <Card key={index} word={word} />
-      ))}
+  const handleSpanishClick = (id) => {
+    setSpanishList(spanishList.filter((item) => item.pers !== id))
+  }
+  const handleEnglishClick = (id) => {
+    setEnglishList(englishList.filter((item) => item.pers !== id))
+  }
+  return (
+    <div className='container'>
+      <h2>Match the Verb Forms</h2>
+      <div className='lists'>
+        <div className='list'>
+          {spanishList.map((word) => (
+            <Card
+              key={word.pers} id={word.pers} word={word.tense}
+              handleClick={(id) => handleSpanishClick(id)}
+            />
+          ))}
+        </div>
+        <div className='list'>
+          {englishList.map((word) => (
+            <Card
+              key={word.pers} id={word.pers} word={word.tense}
+              handleClick={(id) => handleEnglishClick(id)}
+            />
+          ))}
+        </div>
       </div>
     </div>
-  </div>
   )
 }
-export {VerbForm as default}
+export { VerbForm as default }
