f5f8f5867c0527bba2eaed808377f7a00197c82d
Regina McGuire <rmcguire@antelecom.net>
2020-07-21 10:26:11 -0700

    Add database to api

--- api/api.py
+++ api/api.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String
+import os
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'matches.db')
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def homepage():
@@ -9,4 +16,4 @@ def homepage():
 if __name__ == '__main__':
     app.run()
 
-
+ 
