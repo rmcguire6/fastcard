a9313a04710aded5fa629f6e5a426d359e2be16a
Regina McGuire <rmcguire@antelecom.net>
2020-07-21 11:39:25 -0700

    Add Match model

--- api/api.py
+++ api/api.py
@@ -5,13 +5,23 @@ import os
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'matches.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'matches.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 @app.route('/')
 def homepage():
     return('homepage')
+
+# database models
+
+class Match(db.Model):
+    __tablename__ = 'matches'
+    id = Column(Integer, primary_key=True)
+    match_id = Column(String, unique=True)
+    spanish = Column(String, nullable=False)
+    english = Column(String, nullable=False)
 
 if __name__ == '__main__':
     app.run()
