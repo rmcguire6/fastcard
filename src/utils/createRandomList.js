1f4b0992b889260d300d37a72dcdd0fe7eb2b661
Regina McGuire <rmcguire@antelecom.net>
2020-08-12 22:13:48 -0700

    Remove unused console.log

--- src/utils/createRandomList.js
+++ src/utils/createRandomList.js
@@ -3,7 +3,6 @@ import shuffle from 'shuffle-array'
 const createRandomList = (list) => {
   let randomList = list.slice()
   randomList = shuffle(randomList)
-  console.log('randomList is now', randomList)
   return randomList
 }
 export { createRandomList as default }
