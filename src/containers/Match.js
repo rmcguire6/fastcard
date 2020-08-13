a23b4fc5e1431558260a8604ea532ebc35ddb8f3
Regina McGuire <rmcguire@antelecom.net>
2020-08-12 20:06:26 -0700

    Refactor Match.js

--- src/containers/Match.js
+++ src/containers/Match.js
@@ -1,13 +1,13 @@
 import React, { useContext, useEffect, useState } from 'react'
 import createRandomList from '../utils/createRandomList'
 import MatchesContext from '../context/matches-context'
-import Card from '../components/Card'
 import '../App.css'
 
 const Match = () => {
   const { matches } = useContext(MatchesContext)
   const [spanishList, setSpanishList] = useState([])
   const [englishList, setEnglishList] = useState([])
+  const [history, setHistory] = useState([])
 
   useEffect(() => {
     setEnglishList(matches.map((match) => {
@@ -18,31 +18,36 @@ const Match = () => {
     }))
   }, [matches])
   
+  const handleEnglishClick =(item) => {
+    const newId = item.matchId
+    setHistory(history.concat(newId))
+    setEnglishList(englishList.filter((match) => match.matchId !== item.matchId))
+  }
+  const handleSpanishClick =(item) => {
+    const newId = item.matchId
+    setHistory(history.concat(newId))
+    setSpanishList(spanishList.filter((match) => match.matchId !== item.matchId))
+  }
   const shuffled = createRandomList(englishList)
+
   return (
     <div className='container'>
       <h2>Match</h2>
-      <div className='lists'>
-        <div className='list'>
-          {spanishList.map((match) =>
-            <Card
-              key={match.spanish}
-              id={match.matchId}
-              handleClick={(id) => setSpanishList(spanishList.filter((match) => match.matchId !== id))}
-              word={match.spanish}
-            />
-          )}
-        </div>
-        <br />
-        <div className='list'>
-          {shuffled.map((match) =>
-            <Card
-              key={match.english}
-              id={match.matchId}
-              handleClick={(id) => setEnglishList(englishList.filter((list) => list.matchId !== id))}
-              word={`to ${match.english}`}
-            />
-          )}
+        <div className='lists'>
+          <div className='list'>
+            {spanishList.map(match => (
+                <div className='card' key={match.spanish}>
+                  <button className='button' onClick={() => handleSpanishClick(match)}>{match.spanish}</button>
+                </div>
+            ))}
+          </div>
+          <br />
+          <div className='list'>
+            {shuffled.map(match => (
+            <div className='card' key={match.english}>
+            <button className='button' onClick={() => handleEnglishClick(match)}>{`to ${match.english}`}</button>
+          </div>
+          ))}
         </div>
       </div>
     </div>
