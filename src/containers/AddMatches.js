3507ffe1de323a4b489131f4fac7059f017f7694
Regina McGuire <rmcguire@antelecom.net>
2020-09-16 16:01:47 -0700

    Change order of input boxes in AddMatches

--- src/containers/AddMatches.js
+++ src/containers/AddMatches.js
@@ -36,13 +36,14 @@ const AddMatches = () => {
       <form onSubmit={addMatch}>
         <div className='form'>
           <div className='form_input container'>
-            <label htmlFor='english' className='row-text'>English</label>
-            <input name='english' value={english} onChange={(e) => setEnglish(e.target.value)} />
-          </div>
-          <div className='form_input container'>
             <label htmlFor='spanish' className='row-text'>Spanish</label>
             <input name='spanish' value={spanish} onChange={(e) => setSpanish(e.target.value)} />
           </div>
+          <div className='form_input container'>
+            <label htmlFor='english' className='row-text'>English</label>
+            <input name='english' value={english} onChange={(e) => setEnglish(e.target.value)} />
+          </div>
+          
         </div>
         <button className='button'>Add a word pair</button>
       </form>
